f2d4a895328390afa4265f82f1f4120dbd73c11b
Coporton <coporton.workstation@gmail.com>
2023-02-08 20:13:51 +0600

    Docker Builder Main File

---
+++ dockerbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update && apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install -y docker-ce docker-compose
+sudo service docker start
+curl -s https://raw.githubusercontent.com/Coporton/xtreamui-on-docker/master/docker-compose.yml > xui.yml
+docker-compose -f xui.yml up -d
