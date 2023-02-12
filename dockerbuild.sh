fca4e417e98489cda5ebe8236998f8d62e5f6a57
Coporton <coporton.workstation@gmail.com>
2023-02-12 16:25:40 +0600

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,6 +5,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install -y docker-ce docker-compose
 sudo service docker start
-curl -s https://raw.githubusercontent.com/Coporton/xtreamui-on-docker/master/docker-compose.yml > docker-compose.yml
+curl -s https://raw.githubusercontent.com/Coporton/xtreamui-on-docker/main/docker-compose.yml > docker-compose.yml
 docker-compose up -d
 rm -f dockerbuild.sh
