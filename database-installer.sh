e05b025690cdc17a5d405fc782b27bf34b65d30f
Coporton <coporton.workstation@gmail.com>
2023-02-26 12:07:03 +0600

    Create database-installer.sh

---
+++ database-installer.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Check if wget is installed, if not install it
+if ! command -v wget &> /dev/null
+then
+    echo "wget not found, installing..."
+    sudo apt-get update
+    sudo apt-get install wget
+fi
+
+# Check if unzip is installed, if not install it
+if ! command -v unzip &> /dev/null
+then
+    echo "unzip not found, installing..."
+    sudo apt-get update
+    sudo apt-get install unzip
+fi
+
+# URL of the zip file to download
+SQL_URL="http://example.com/file.zip"
+
+# Target directory to extract the zip file to
+TARGET_DIR="/opt/xtreamcodes/iptv_xtream_codes/*"
+
+# Check if target directory exists
+if [ -d "$TARGET_DIR" ]; then
+    read -p "The target directory $TARGET_DIR already exists. Do you want to replace it? [y/N] " choice
+    case "$choice" in
+        y|Y ) echo "Replacing $TARGET_DIR..."; rm -rf "$TARGET_DIR";;
+        * ) echo "Aborting operation..."; exit 1;;
+    esac
+fi
+
+# Download the zip file using wget
+wget "$SQL_URL" -O /tmp/mysql-database.zip
+
+# Extract the zip file to /opt/
+unzip /tmp/file.zip -d "$TARGET_DIR"
+
+# Set file and folder permissions to 108:112 recursively
+chmod -R 108:112 "$TARGET_DIR"
+
+# Remove the script file
+rm "$0"
