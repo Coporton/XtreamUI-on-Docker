6b1777b07269bc0959e7691ae0fc2c8e45515491
Coporton <89565607+Coporton@users.noreply.github.com>
2023-02-28 00:28:50 +0600

    Update main-installer.sh

--- main-installer.sh
+++ main-installer.sh
@@ -8,18 +8,9 @@ echo "xtreamui database is installing..."
 sleep 1
 docker pull coporton/xtreamui:latest
 sleep 1
-docker run -d \
-  --name xtreamui \
-  coporton/xtreamui:latest \
-  -p 25461:25461 \
-  -p 25462:25462 \
-  -p 25463:25463 \
-  -p 25464:25464 \
-  -p 25465:25465 \
-  -p 25500:25500 \
-  -v /opt/xtreamcodes/iptv_xtream_codes/logs:/home/xtreamcodes/iptv_xtream_codes/logs \
-  -v /opt/xtreamcodes/iptv_xtream_codes/mysql:/var/lib/mysql \
-  /usr/bin/supervisord
+
+docker run -d -p 25462:25462 -p 25461:25461 -p 25463:25463 -p 25464:25464 -p 25465:25465 -p 25500:25500 -v /opt/xtreamcodes/iptv_xtream_codes/logs:/home/xtreamcodes/iptv_xtream_codes/logs -v /opt/xtreamcodes/iptv_xtream_codes/mysql:/var/lib/mysql --name xtreamui coporton/xtreamui:latest /usr/bin/supervisord
+
 echo "The web interface is loading, it will take less than a minute. Please wait..."
 # Remove the script file
 rm "$0"
