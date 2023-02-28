fba69cc68edf875780ddba2a5836704fe8e184e2
Coporton <coporton.workstation@gmail.com>
2023-02-28 17:14:51 +0600

    Update stop-xtream.sh

--- stop-xtream.sh
+++ stop-xtream.sh
@@ -4,12 +4,18 @@ GNCOL='\e[40;38;5;82m' #GNCOL
 
 CONTAINER_NAME="xtreamui"
 
-docker stop $CONTAINER_NAME
-
-echo " "
-echo -e "${GNCOL} ┌─────────────────────────────────────┐ \e[0m"
-echo -e "${GNCOL} │     xtreamui docker is stoped...    │ \e[0m"
-echo -e "${GNCOL} └─────────────────────────────────────┘ \e[0m"
-echo " "
-
+if [ "$(docker ps -q -f name=$CONTAINER_NAME -f status=exited)" ]; then
+    echo " "
+    echo -e "${GNCOL} ┌─────────────────────────────────────┐ \e[0m"
+    echo -e "${GNCOL} │    $CONTAINER_NAME is already stopped.     │ \e[0m"
+    echo -e "${GNCOL} └─────────────────────────────────────┘ \e[0m"
+    echo " "
+else
+    echo " "
+    echo -e "${GNCOL} ┌─────────────────────────────────────┐ \e[0m"
+    echo -e "${GNCOL} │        $CONTAINER_NAME is stopped.         │ \e[0m"
+    echo -e "${GNCOL} └─────────────────────────────────────┘ \e[0m"
+    echo " "
+    docker stop $CONTAINER_NAME
+fi
 exit 0
