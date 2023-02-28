23716720f2c8d26ff9b1fbaaddc330f0ab67b646
Coporton <coporton.workstation@gmail.com>
2023-02-28 17:14:48 +0600

    Update start-xtream.sh

--- start-xtream.sh
+++ start-xtream.sh
@@ -2,12 +2,19 @@
 echo " "
 GNCOL='\e[40;38;5;82m' #GNCOL
 
-echo " "
-echo -e "${GNCOL} ┌─────────────────────────────────────┐ \e[0m"
-echo -e "${GNCOL} │   xtreamui docker is starting...    │ \e[0m"
-echo -e "${GNCOL} └─────────────────────────────────────┘ \e[0m"
-echo " "
-
 CONTAINER_NAME="xtreamui"
 
-docker start $CONTAINER_NAME
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo " "
+    echo -e "${GNCOL} ┌─────────────────────────────────────┐ \e[0m"
+    echo -e "${GNCOL} │    $CONTAINER_NAME is already running.     │ \e[0m"
+    echo -e "${GNCOL} └─────────────────────────────────────┘ \e[0m"
+    echo " "
+else
+    echo " "
+    echo -e "${GNCOL} ┌─────────────────────────────────────┐ \e[0m"
+    echo -e "${GNCOL} │   $CONTAINER_NAME docker is starting...    │ \e[0m"
+    echo -e "${GNCOL} └─────────────────────────────────────┘ \e[0m"
+    echo " "
+    docker start $CONTAINER_NAME
+fi
